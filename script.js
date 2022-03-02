06cdee970876131cbda04cf85d67724257c5026e
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-02 17:14:15 +0100

    cursor pointer

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 document.querySelector('header button').addEventListener('click', function () {
-    document.getElementById("container").innerHTML = '';
-    for (let i = 1; i <= 100; i++) {
-        const element = document.createElement('div');
-        switch (document.querySelector("select").value) {
-            case "1":
-                if (i <= 100) {
-                    element.innerHTML = i;
-                }
-                break;
-            case "2":
-                if (i <= 81) {
-                    element.innerHTML = i;
-                }
-                break;
-            case "3":
-                if (i <= 49) {
-                    element.innerHTML = i;
-                }
-                break;
-        }
-        element.classList.add('square');
-        document.getElementById("container").append(element);
+    document.getElementById("container").innerHTML='';
+    
+    switch (document.querySelector("select").value) {
+        case "1":
+            for (let i = 1; i <= 100; i++) {
+                const element = document.createElement('div');
+                element.classList.add('square', 'easy');
+                element.innerHTML = i;
+                document.getElementById("container").append(element);
+            }
+        break;
+        case "2":
+            for (let i = 1; i <= 81; i++) {
+                const element = document.createElement('div');
+                element.classList.add('square', 'medium');
+                element.innerHTML = i;
+                document.getElementById("container").append(element);
+            }
+        break;
+        case "3":
+            for (let i = 1; i <= 49; i++) {
+                const element = document.createElement('div');
+                element.classList.add('square', 'hard');
+                element.innerHTML = i;
+                document.getElementById("container").append(element);
+            }
+        break;    
     }
 })

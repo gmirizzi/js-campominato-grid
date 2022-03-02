e55bda98c9c1adbee195134dde3431b1e7683a0f
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-02 15:57:05 +0100

    aggiunti i  tre livelli

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-for (let i = 1; i <= 100; i++) {
-    const element = document.createElement('div');
-    element.classList.add('square');
-    element.innerHTML = i;
-    document.getElementById("container").append(element);
-}
+document.querySelector('header button').addEventListener('click', function () {
+    document.getElementById("container").innerHTML=''
+    switch (document.querySelector("select").value) {
+        case "1":
+            for (let i = 1; i <= 100; i++) {
+                const element = document.createElement('div');
+                element.classList.add('square');
+                element.innerHTML = i;
+                document.getElementById("container").append(element);
+            }
+        break;
+        case "2":
+            for (let i = 1; i <= 81; i++) {
+                const element = document.createElement('div');
+                element.classList.add('square');
+                element.innerHTML = i;
+                document.getElementById("container").append(element);
+            }
+        break;
+        case "3":
+            for (let i = 1; i <= 49; i++) {
+                const element = document.createElement('div');
+                element.classList.add('square');
+                element.innerHTML = i;
+                document.getElementById("container").append(element);
+            }
+        break;    
+    }
+})

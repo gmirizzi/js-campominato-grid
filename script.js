00668df4455fd5b74043b22e5496d321f9c46db0
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-03 09:53:36 +0100

    implementata classe active e invertite difficoltà

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ document.querySelector('header button').addEventListener('click', function () {
     document.getElementById("container").innerHTML='';
     
     switch (document.querySelector("select").value) {
-        case "1":
+        case "3":
             for (let i = 1; i <= 100; i++) {
                 const element = document.createElement('div');
+                element.addEventListener('click', function() {
+                    element.classList.add('active');
+                })
                 element.classList.add('square', 'easy');
                 element.innerHTML = i;
                 document.getElementById("container").append(element);
@@ -13,14 +16,20 @@ document.querySelector('header button').addEventListener('click', function () {
         case "2":
             for (let i = 1; i <= 81; i++) {
                 const element = document.createElement('div');
+                element.addEventListener('click', function() {
+                    element.classList.add('active');
+                })
                 element.classList.add('square', 'medium');
                 element.innerHTML = i;
                 document.getElementById("container").append(element);
             }
         break;
-        case "3":
+        case "1":
             for (let i = 1; i <= 49; i++) {
                 const element = document.createElement('div');
+                element.addEventListener('click', function() {
+                    element.classList.add('active');
+                })
                 element.classList.add('square', 'hard');
                 element.innerHTML = i;
                 document.getElementById("container").append(element);

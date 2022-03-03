511950d2c6bc8caea4d3bca71736cf3f3d63382c
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-03 14:44:25 +0100

    inserite le bombe a tutti i livelli

--- script.js
+++ script.js
@@ -3,54 +3,72 @@ document.querySelector('header button').addEventListener('click', function () {
     
     switch (document.querySelector("select").value) {
         case "1":
+            bombe=bombGenerator(100);
             for (let i = 1; i <= 100; i++) {
                 const element = document.createElement('div');
                 element.addEventListener('click', function() {
-                    this.classList.add('active');
+                    for (let index = 0; index < bombe.length; index++) {
+                        if (this.innerHTML==bombe[index]){
+                            this.classList.add('bomb')
+                        } else {
+                            this.classList.add('active');
+                        }
+                    }
                 })
                 element.classList.add('square', 'easy');
                 element.innerHTML = i;
                 document.getElementById("container").append(element);
             }
-            bombGenerator(100);
         break;
         case "2":
+            bombe=bombGenerator(81);
             for (let i = 1; i <= 81; i++) {
                 const element = document.createElement('div');
                 element.addEventListener('click', function() {
-                    this.classList.add('active');
+                    for (let index = 0; index < bombe.length; index++) {
+                        if (this.innerHTML==bombe[index]){
+                            this.classList.add('bomb')
+                        } else {
+                            this.classList.add('active');
+                        }
+                    }
                 })
                 element.classList.add('square', 'medium');
                 element.innerHTML = i;
                 document.getElementById("container").append(element);
             }
-            bombGenerator(81);
         break;
         case "3":
+            bombe=bombGenerator(49);
             for (let i = 1; i <= 49; i++) {
                 const element = document.createElement('div');
                 element.addEventListener('click', function() {
-                    this.classList.add('active');
+                    for (let index = 0; index < bombe.length; index++) {
+                        if (this.innerHTML==bombe[index]){
+                            this.classList.add('bomb')
+                        } else {
+                            this.classList.add('active');
+                        }
+                    }
                 })
                 element.classList.add('square', 'hard');
                 element.innerHTML = i;
                 document.getElementById("container").append(element);
             }
-            bombGenerator(49);
         break;    
     }
 })
 
 function bombGenerator(max){
-    const bombe = [];
+    const array = [];
     let i= 0;
     while (i<16) {
         num = parseInt(Math.random() * max + 1); 
-        if (!bombe.includes(num)) {
-            bombe.push(num);            
+        if (!array.includes(num)) {
+            array.push(num);            
             i++;
         };
     }
-    console.log(bombe)
+    return array;
 }
 
